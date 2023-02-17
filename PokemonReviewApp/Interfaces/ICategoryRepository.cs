49acabfd677ac317b9de3d4db3c3c12bc1c74418
João Victor Sperandio <joaovictorapsperandio@gmail.com>
2023-02-17 19:44:33 -0300

    Pokemon API

---
+++ PokemonReviewApp/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        // For Read Functions
+        ICollection<Category> GetCategories();
+        Category GetCategory(int id);
+        ICollection<Pokemon> GetPokemonByCategory(int categoryId);
+        bool CategoryExists(int id);
+        // For Create Functions
+        bool CreateCategory(Category category);
+        bool UpdateCategory(Category category);
+        bool DeleteCategory(Category category);
+        bool Save();
+    }
+}
---
+++ PokemonReviewApp/Interfaces/ICountryRepository.cs
@@ -0,0 +1,19 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interfaces
+{
+    public interface ICountryRepository
+    {
+        // For Read Functions
+        ICollection<Country> GetCountries();
+        Country GetCountry(int id);
+        Country GetCountryByOwner(int ownerId);
+        ICollection<Owner> GetOwenersFromACountry(int countryId);
+        bool CountryExists(int id);
+        // For Create Functions
+        bool CreateCountry(Country country);
+        bool UpdateCountry(Country country);
+        bool DeleteCountry(Country country);
+        bool Save();
+    }
+}
